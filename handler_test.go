e7a59405b0b656f1508c57ae4963b6ee66e38aa5
Andrey Blinov <kodnaplakal@gmail.com>
2018-03-09 18:21:51 +0200

    Add test config

--- handler_test.go
+++ handler_test.go
@@ -1,10 +1,6 @@
 package geoip
 
-import (
-	"testing"
-)
-
-func TestInvokeOK(t *testing.T) {
-	//gip := GeoIP{}
-	//caddy.NewTestController("http", test.input)
-}
+// func TestInvokeOK(t *testing.T) {
+// 	//gip := GeoIP{}
+// 	//caddy.NewTestController("http", test.input)
+// }
